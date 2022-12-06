0c372d9008f95e6f8c0532047246be1985b9e903
Diego Tutin <diegotutin3255@gmail.com>
2022-12-06 17:47:14 -0500

    Actulizacion Dashboard y una tabla jugadores

---
+++ database/migrations/2022_12_06_215543_create_equipos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEquiposTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipos', function (Blueprint $table) {
+            $table->id();
+            $table->integer('idEquipo');
+            $table->string('nombres');
+            $table->string('apellidos');
+            $table->string('cedula');
+            $table->string('telefono');
+            $table->string('correo');
+            $table->text('observaciones');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipos');
+    }
+}
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -616,6 +616,7 @@ class ComposerStaticInitf32512016c9c676c700ee7ee604d9d4d
         'App\\Http\\Middleware\\TrustProxies' => __DIR__ . '/../..' . '/app/Http/Middleware/TrustProxies.php',
         'App\\Http\\Middleware\\VerifyCsrfToken' => __DIR__ . '/../..' . '/app/Http/Middleware/VerifyCsrfToken.php',
         'App\\Models\\User' => __DIR__ . '/../..' . '/app/Models/User.php',
+        'App\\Models\\equipos' => __DIR__ . '/../..' . '/app/Models/equipos.php',
         'App\\Providers\\AppServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AppServiceProvider.php',
         'App\\Providers\\AuthServiceProvider' => __DIR__ . '/../..' . '/app/Providers/AuthServiceProvider.php',
         'App\\Providers\\BroadcastServiceProvider' => __DIR__ . '/../..' . '/app/Providers/BroadcastServiceProvider.php',
